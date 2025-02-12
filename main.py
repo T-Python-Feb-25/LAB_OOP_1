97c3554b390105ffa22014ce0a3dee71feef2bff
Areej <areej.a7676@gmail.com>
2025-02-12 16:50:04 +0300

    Add lab

---
+++ main.py
@@ -0,0 +1,22 @@
+
+from panda import Panda
+
+#Create 4 instances/objects
+Panda1 = Panda("Fluffy", "Famale", 5, 105.5)
+Panda2 = Panda("Lian", "Famale", 4, 95.5)
+Panda3 = Panda("Bao", "Male", 8, 140.5)
+Panda4 = Panda("Bamboo", "Male", 6, 125.5)
+
+#استدعا
+Panda1.play()
+Panda1.eat()
+
+Panda2.play()
+Panda2.eat
+
+Panda3.play()
+Panda3.eat()
+
+Panda4.play()
+Panda4.eat
+
---
+++ panda.py
@@ -0,0 +1,21 @@
+class Panda:
+     #constructor
+    def __init__(self, name:str, gender:str, age:int, weight:float):
+        #addtributes
+        self.name = name
+        self.gender = gender
+        self.age = age
+        self.wieght = weight
+
+        #objects
+    def play(self):
+        print(f"{self.name} is playing happily!")
+
+    def eat(self):
+        print(f"{self.name} is eating bamboo!")
+
+
+
+
+
+        
