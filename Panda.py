89bccefd392fe9872341c9a18e44c8cd818dda5d
MohammedAAlbushaier <moh.albushaier@gmail.com>
2025-02-12 19:13:19 +0300

    Commiting the LAB_OOP_1

---
+++ Panda.py
@@ -0,0 +1,19 @@
+'''
+Using what you learned about classes , create a class to represent a Panda. The class should have the following:
+4 Attributes
+2 Methods
+'''
+# panda.py
+
+class Panda:
+    def __init__(self, name, age, weight, habits):
+        self.name = name
+        self.age = age
+        self.weight = weight
+        self.habits = habits
+
+    def weights(self):
+        return f"{self.name} weights {self.weight}."
+
+    def add_habits(self):
+        return f"{self.name} is enjoying his leisure time {self.habits}."
---
+++ main.py
@@ -0,0 +1,32 @@
+'''
+Create 4 instances/objects of the class Panda , print 1 attribute value, and call the two methods on each instance.
+Note:
+Arrange your code in seperate files (one for the main file for main logic , and another for the definition of the class).
+'''
+from Panda import Panda
+
+
+#(name, age, weight, habits)
+panda1 = Panda("Ping Pan", 10, 250, "Playing With a stick")
+panda2 = Panda("Bao Bao", 3, 120, "Climbing")
+panda3 = Panda("Mei Mei", 6, 160, "Sleeping")
+panda4 = Panda("Tian Tian", 4, 140, "Running")
+
+# Print one attribute value (name) of each panda
+print(panda1.name)
+print(panda2.name)
+print(panda3.name)
+print(panda4.name)
+
+# Call the methods on each instance
+print(panda1.weights())
+print(panda1.add_habits())
+
+print(panda2.weights())
+print(panda2.add_habits())
+
+print(panda3.weights())
+print(panda3.add_habits())
+
+print(panda4.weights())
+print(panda4.add_habits())
